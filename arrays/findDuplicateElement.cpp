8a65d75e3db1e595bf17602571538b87e6947a7d
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-03-15 13:51:36 +0530

    minimize the heights 2

--- arrays/findDuplicateElement.cpp
+++ arrays/findDuplicateElement.cpp
@@ -1,5 +1,12 @@
+// https : // leetcode.com/problems/find-the-duplicate-number/description/
+// 15/3/2023
+
+// Given an array of integers nums containing n + 1 integers where each integer is in the range[1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
 #include <bits/stdc++.h>
-using namespace std;
+           using namespace std;
 int findDuplicate(vector<int> &nums)
 {
     int slow = nums[0];
