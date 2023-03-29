aa28bca2bea2f323c54bbff6ea666c8696c27dfd
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-03-30 03:38:26 +0530

    creating graph and bfs traversal of a graph

---
+++ graph/bfsTraversal.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> bfsOfGraph(int V, vector<int> adj[])
+{
+    vector<int> ans;
+    queue<int> q;
+    q.push(0);
+    vector<int> vis(V, 0);
+    vis[0] = 1;
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        for (auto c : adj[node])
+        {
+            if (!vis[c])
+            {
+                vis[c] = 1;
+                q.push(c);
+            }
+        }
+
+        ans.push_back(node);
+    }
+    return ans;
+}
