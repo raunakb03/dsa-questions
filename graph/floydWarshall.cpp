b1bc0dc9ea3631d5e3cd9e6586545a65f2a68943
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-09 16:47:14 +0530

    floyd warshall algorithm

---
+++ graph/floydWarshall.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// to find if the negative edge cycle exists or not check if any of the [i][i] have a value not equal to zero
+
+void shortest_distance(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == -1)
+                matrix[i][j] = 1e8;
+        }
+    }
+
+    for (int k = 0; k < n; k++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == 1e8)
+                matrix[i][j] = -1;
+        }
+    }
+}
