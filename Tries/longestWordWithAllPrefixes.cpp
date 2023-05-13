a01b750c4844baeb434cc983f913799f91e2ccd0
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-13 19:14:18 +0530

    longest word with all prefixes

---
+++ Tries/longestWordWithAllPrefixes.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    Node *links[26];
+    bool flag = false;
+
+    bool containsKey(char ch)
+    {
+        return (links[ch - 'a'] != NULL);
+    }
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
+    }
+    void setEnd()
+    {
+        flag = true;
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+
+public:
+    bool checkIfPrefixExists(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (node->containsKey(word[i]))
+            {
+                node = node->get(word[i]);
+                if (node->isEnd() == false)
+                    return false;
+            }
+            return false;
+        }
+        return true;
+    }
+};
+
+string completeString(int n, vector<string> &a)
+{
+    Trie trie;
+    for (auto &it : a)
+    {
+        trie.insert(it);
+    }
+
+    string longest = "";
+    for (auto &it : a)
+    {
+        if (trie.checkIfPrefixExists(it))
+        {
+            if (it.size() > longest.size())
+            {
+                longest = it;
+            }
+            else if (it.size() == longest.size() && it < longest)
+            {
+                longest = it;
+            }
+        }
+    }
+
+    if (longest == "")
+        return "NONE";
+    return longest;
+}
