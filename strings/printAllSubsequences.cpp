cf3966ac8969d5b6d87d4a78a815305221ff9366
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-17 00:07:17 +0530

    print all subequences of a string

---
+++ strings/printAllSubsequences.cpp
@@ -0,0 +1,13 @@
+// C++ program for the above approach
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void printSubsequence(string input, string output)
+{
+    if (input.empty()) {
+        cout << output << endl;
+        return;
+    }
+    printSubsequence(input.substr(1), output + input[0]);
+    printSubsequence(input.substr(1), output);
+}
