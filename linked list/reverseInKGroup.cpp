6d5be998d233e40ade713a776c176ef9fbbf7077
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-18 09:51:39 +0530

    reverse linked list in group of k using iterative method

--- linked list/reverseInKGroup.cpp
+++ linked list/reverseInKGroup.cpp
@@ -35,3 +35,39 @@ ListNode *reverseKGroup(ListNode *head, int k)
     head->next = reverseKGroup(curr, k);
     return prev;
 }
+
+// using iterative approach
+ListNode *reverseKGroup(ListNode *head, int k)
+{
+    ListNode *dummy = new ListNode(0);
+    dummy->next = head;
+    ListNode *before = dummy;
+    ListNode *after = head;
+    ListNode *curr = NULL;
+    ListNode *prev = NULL;
+    ListNode *nxt = NULL;
+
+    while (true)
+    {
+        ListNode *cursor = after;
+        for (int i = 0; i < k; i++)
+        {
+            if (cursor == NULL)
+                return dummy->next;
+            cursor = cursor->next;
+        }
+        curr = after;
+        prev = before;
+        for (int i = 0; i < k; i++)
+        {
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        after->next = curr;
+        before->next = prev;
+        before = after;
+        after = curr;
+    }
+}
