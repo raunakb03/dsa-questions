d51c7261018b30f7a43d7ac1d62a5953781f9722
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-20 21:52:35 +0530

    sliding window maximum

--- stack & queue/slidingWindowMaximum[VVIP].cpp
+++ stack & queue/slidingWindowMaximum[VVIP].cpp
@@ -1,21 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int maxSlidingWindow(vector<int> &nums, int k)
-{
-    deque<int> dq;
-    vector<int> ans;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (!dq.empty() && dq.front() == i - k)
-            dq.pop_front();
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++){
+            if(!dq.empty() && dq.front()==i-k)
+                dq.pop_front();
 
-        while (!dq.empty() && nums[dq.back()] < nums[i])
-            dq.pop_back();
+            while(!dq.empty() && nums[dq.back()]<nums[i])
+                dq.pop_back();
 
-        dq.push_back(i);
-        if (i >= k - 1)
-            ans.push_back(nums[dq.front()]);
+            dq.push_back(i);
+            if(i>=k-1)
+                ans.push_back(nums[dq.front()]);
+        }
+        return ans;
     }
-    return ans;
-}
