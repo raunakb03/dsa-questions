96b09183b17220cda4b10a9a6172fde2d78452b5
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-26 19:15:04 +0530

    minimum number of swaps to sort the array

---
+++ searching & sorting/minSwapsToSortTheArray.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minSwaps(vector<int> &nums)
+{
+    vector<int> temp = nums;
+    sort(nums.begin(), nums.end());
+    map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        mp[temp[i]] = i;
+    }
+    int cnt = 0, f = 0;
+    map<int, int> vis;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == temp[i])
+        {
+            vis[nums[i]] = 1;
+        }
+        if (!vis[nums[i]])
+        {
+            int ele = nums[i];
+            f = 0;
+            while (true)
+            {
+                f++;
+                if (!vis[ele])
+                {
+                    vis[ele] = 1;
+                    ele = nums[mp[ele]];
+                }
+                else
+                    break;
+            }
+            f--;
+            cnt += (f - 1);
+        }
+    }
+    return cnt;
+}
