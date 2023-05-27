84f3c912654adf17579f8d9909308dbf42ba7b53
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-05-27 20:21:47 +0530

    matrix chain multipliaction

--- dp/mcm.cpp
+++ dp/mcm.cpp
@@ -1,6 +1,7 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+// memoization
 int f(int i, int j, int arr[], vector<vector<int>> &dp)
 {
     if (i == j)
@@ -20,4 +21,28 @@ int matrixMultiplication(int N, int arr[])
 {
     vector<vector<int>> dp(N, vector<int>(N, -1));
     return f(1, N - 1, arr, dp);
+}
+
+// recursive approach
+int matrixMultiplication(int N, int arr[])
+{
+    int dp[N][N] = {0};
+
+    for (int i = 0; i < N; i++)
+        dp[i][i] = 0;
+    for (int i = N - 1; i >= 1; i--)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            int mini = INT_MAX;
+            for (int k = i; k <= j - 1; k++)
+            {
+                int steps = dp[i][k] + dp[k + 1][j] + arr[i - 1] * arr[k] * arr[j];
+                mini = min(steps, mini);
+            }
+
+            dp[i][j] = mini;
+        }
+    }
+    return dp[1][N - 1];
 }
