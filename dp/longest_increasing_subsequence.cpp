dc0d9ed061773a013818d2188e55c6c030f33218
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-06-01 17:08:18 +0530

    finding the length of longest increasing subsequence and printing it

--- dp/longest_increasing_subsequence.cpp
+++ dp/longest_increasing_subsequence.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// memoization
 int f(int ind, int prev_ind, vector<int> &nums, vector<vector<int>> &dp)
 {
     int n = nums.size();
@@ -15,6 +16,8 @@ int f(int ind, int prev_ind, vector<int> &nums, vector<vector<int>> &dp)
 
     return dp[ind][prev_ind + 1] = max(include, notInclude);
 }
+
+// tabulation
 int lengthOfLIS(vector<int> &nums)
 {
     int n = nums.size();
@@ -33,4 +36,65 @@ int lengthOfLIS(vector<int> &nums)
         }
     }
     return dp[0][0];
+}
+
+// method 3
+// dp[i] denotes the max length of increasing subsequence ending at index i.
+int lengthOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> dp(n, 1), hash(n);
+    int lastInd = 0;
+    int maxi = 1;
+    for (int i = 0; i < n; i++)
+    {
+        hash[i] = i;
+        for (int prev = 0; prev < i; prev++)
+        {
+            if (nums[prev] < nums[i] && 1 + dp[prev] > dp[i])
+            {
+                dp[i] = 1 + dp[prev];
+                hash[i] = prev;
+            }
+        }
+        if (dp[i] > maxi)
+        {
+            maxi = dp[i];
+            lastInd = i;
+        }
+    }
+
+    // getting the longest increasing subsequence
+    vector<int> lis(maxi);
+    lis[0] = nums[lastInd];
+    int ind = 1;
+    while (hash[lastInd] != lastInd)
+    {
+        lastInd = hash[lastInd];
+        lis[ind++] = lastInd;
+    }
+
+    reverse(lis.begin(), lis.end());
+    return maxi;
+}
+
+// binary search approach
+int lengthOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> temp;
+    temp.push_back(nums[0]);
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] > temp.back())
+        {
+            temp.push_back(nums[i]);
+        }
+        else
+        {
+            int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+            temp[ind] = nums[i];
+        }
+    }
+    return temp.size();
 }
