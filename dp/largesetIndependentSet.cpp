420e937bf12a3fb7c49ad4d9a4ab8e3a9ba1c088
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-06-06 10:09:49 +0530

    largest independent set problem

---
+++ dp/largesetIndependentSet.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int max(int x, int y) { return (x > y) ? x : y; }
+
+class node
+{
+public:
+    int data;
+    int liss;
+    node *left, *right;
+};
+
+int LISS(node *root)
+{
+    if (root == NULL)
+        return 0;
+
+    if (root->liss)
+        return root->liss;
+
+    if (root->left == NULL && root->right == NULL)
+        return (root->liss = 1);
+
+    int liss_excl = LISS(root->left) + LISS(root->right);
+
+    int liss_incl = 1;
+    if (root->left)
+        liss_incl += LISS(root->left->left) + LISS(root->left->right);
+    if (root->right)
+        liss_incl += LISS(root->right->left) + LISS(root->right->right);
+
+    root->liss = max(liss_incl, liss_excl);
+
+    return root->liss;
+}
