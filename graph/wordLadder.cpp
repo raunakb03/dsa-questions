b393aca9141dd78e6f383d8947e04f3d60a2edde
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-06-14 23:06:46 +0530

    word ladder

---
+++ graph/wordLadder.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+{
+    // begin word to end word with the shortest sequence
+    queue<pair<string, int>> q;
+    q.push({beginWord, 1});
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    while (!q.empty())
+    {
+        string word = q.front().first;
+        int steps = q.front().second;
+        q.pop();
+
+        if (word == endWord)
+            return steps;
+        for (int i = 0; i < word.size(); i++)
+        {
+            char original = word[i];
+            for (char ch = 'a'; ch <= 'z'; ch++)
+            {
+                word[i] = ch;
+                if (st.find(word) != st.end())
+                {
+                    st.erase(word);
+                    q.push({word, steps + 1});
+                }
+            }
+            word[i] = original;
+        }
+    }
+    return 0;
+}
