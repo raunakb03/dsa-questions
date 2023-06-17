f19ec3c4867994ff1be44fc791b8f863cbb9bc4e
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-06-17 13:03:47 +0530

    reverse linked list in range

---
+++ linked list/reverseLinkedListInRange.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+    ListNode(int d)
+    {
+        this->val = d;
+        this->next = NULL;
+    }
+};
+ListNode *reverseBetween(ListNode *head, int m, int n)
+{
+    ListNode *dummy = new ListNode(0), *pre = dummy, *cur;
+    dummy->next = head;
+    for (int i = 0; i < m - 1; i++)
+    {
+        pre = pre->next;
+    }
+    cur = pre->next;
+    for (int i = 0; i < n - m; i++)
+    {
+        ListNode *temp = pre->next;
+        pre->next = cur->next;
+        cur->next = cur->next->next;
+        pre->next->next = temp;
+    }
+    return dummy->next;
+}
